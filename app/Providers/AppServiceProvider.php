9ebc54380daf30d7a51fe8dc675e2383626f0fad
Mochamad Nor Fadillah <mochamadfadilah88@gmail.com>
2023-01-06 13:57:18 +0700

    feat: add package laravel-ide-helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (App::environment('local')) {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 
     /**
