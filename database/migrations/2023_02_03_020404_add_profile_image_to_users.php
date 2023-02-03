8753e63a4adedd18f87d97d9f33a9f2e09e92bc9
Mochamad Nor Fadillah <mochamadfadilah88@gmail.com>
2023-02-03 09:28:39 +0700

    fix: change profil pic name to avatar

--- database/migrations/2023_02_03_020404_add_profile_image_to_users.php
+++ database/migrations/2023_02_03_020404_add_profile_image_to_users.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('profile_image')->nullable()->after('password');
+            $table->string('avatar')->nullable()->after('password');
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('profile_image');
+            $table->dropColumn('avatar');
         });
     }
 };
